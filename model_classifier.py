8142fa572c4f3c12744fbfad45fbb22d3e406f62
Hong Jing (Jingles) <jinglescode@gmail.com>
2022-09-20 16:21:35 +0800

    Update model_classifier.py

--- model_classifier.py
+++ model_classifier.py
@@ -1,5 +1,6 @@
 import torch
 from torch import nn
+from .Multitask import MultitaskSSVEP
 
 
 class MultitaskSSVEPClassifier(nn.Module):
@@ -7,14 +8,12 @@ class MultitaskSSVEPClassifier(nn.Module):
     Using multi-task learning to capture signals simultaneously from the fovea efficiently and the neighboring targets in the peripheral vision generate a visual response map. A calibration-free user-independent solution, desirable for clinical diagnostics. A stepping stone for an objective assessment of glaucoma patients’ visual field.
     Learn more about this model at https://jinglescode.github.io/ssvep-multi-task-learning/
     This model is a multi-class classifier.
-
     Usage:
         model = MultitaskSSVEPClassifier(
             num_channel=11,
             num_classes=40,
             signal_length=250,
         )
-
         x = torch.randn(2, 11, 250)
         print("Input shape:", x.shape) # torch.Size([2, 11, 250])
         y = model(x)
@@ -33,46 +32,14 @@ class MultitaskSSVEPClassifier(nn.Module):
     
     def __init__(self, num_channel=10, num_classes=4, signal_length=1000, filters_n1=4, kernel_window_ssvep=59, kernel_window=19, conv_3_dilation=4, conv_4_dilation=4):
         super().__init__()
-
-        filters = [filters_n1, filters_n1 * 2]
-
-        self.conv_1 = Conv2dBlockELU(in_ch=1, out_ch=filters[0], kernel_size=(1, kernel_window_ssvep), w_in=signal_length)
-        self.conv_2 = Conv2dBlockELU(in_ch=filters[0], out_ch=filters[0], kernel_size=(num_channel, 1))
-        self.conv_3 = Conv2dBlockELU(in_ch=filters[0], out_ch=filters[1], kernel_size=(1, kernel_window), padding=(0,conv_3_dilation-1), dilation=(1,conv_3_dilation), w_in=self.conv_1.w_out)
-        self.conv_4 = Conv2dBlockELU(in_ch=filters[1], out_ch=filters[1], kernel_size=(1, kernel_window), padding=(0,conv_4_dilation-1), dilation=(1,conv_4_dilation), w_in=self.conv_3.w_out)
-        self.conv_classifier = nn.Conv2d(filters[1], num_classes, kernel_size=(1, self.conv_4.w_out))
-
-        self.dropout = nn.Dropout(p=0.5)
+        self.base = MultitaskSSVEP(num_channel, num_classes, signal_length, filters_n1, kernel_window_ssvep, kernel_window, conv_3_dilation, conv_4_dilation)
+        self.fc = nn.Linear(num_classes*2, out_features=num_classes)
 
     def forward(self, x):
-        x = torch.unsqueeze(x,1)
-
-        x = self.conv_1(x)
-        x = self.conv_2(x)
-        x = self.dropout(x)
-
-        x = self.conv_3(x)
-        x = self.conv_4(x)
-        x = self.dropout(x)
-
-        x = self.conv_classifier(x)
-        return x.squeeze()
-
-
-class Conv2dBlockELU(nn.Module):
-    def __init__(self, in_ch, out_ch, kernel_size, padding=(0,0), dilation=(1,1), groups=1, w_in=None):
-        super(Conv2dBlockELU, self).__init__()
-        self.conv = nn.Sequential(
-            nn.Conv2d(in_ch, out_ch, kernel_size=kernel_size, padding=padding, dilation=dilation, groups=groups),
-            nn.BatchNorm2d(out_ch),
-            nn.ELU(inplace=True)
-        )
-
-        if w_in is not None:
-            self.w_out = int( ((w_in + 2 * padding[1] - dilation[1] * (kernel_size[1]-1)-1) / 1) + 1 )
-
-    def forward(self, x):
-        return self.conv(x)
+        x = self.base(x)
+        x = torch.flatten(x, start_dim=1)
+        x = self.fc(x)
+        return x
 
 
 def test():
